eec6c0c7d5b83897a644bb4c5b70fab17f42f7c2
Son Phan <sonphan@Sons-MacBook-Air-2.local>
2023-10-12 09:30:19 -0400

    Fix Issue #5

---
+++ Column.java
@@ -0,0 +1,43 @@
+package ScrumProjectCode;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Column{
+
+    public String ColumnName;
+    public ArrayList<Task> ColumnTasks;
+    private UUID columnUUID;
+
+    public Column(String Category, Task task)
+    {
+
+    }
+
+    private void createColumn(String columnName)
+    {
+
+    }
+
+    private UUID generateUUID()
+    {
+
+    }
+
+    private void addTask(Task task)
+    {
+
+    }
+
+    private void removeTask(Task task)
+    {
+
+    }
+
+    public String toString()
+    {
+
+    }
+
+    
+}
