beb748363e5abe0fbab634e0abd2f72201d5553f
AidanH002 <112006206+AidanH002@users.noreply.github.com>
2023-10-13 11:23:08 -0400

    Fixed #14 and #15

---
+++ scrumprojectcode/Documentation.java
@@ -0,0 +1,35 @@
+package scrumprojectcode;
+
+public class Documentation {
+
+    // Attributes
+    private ArrayList<String> documentation;
+
+    // Constructor
+    public Documentation(ArrayList<String> documentation) {
+        // TODO: Constructor logic
+        this.documentation = documentation;
+    }
+
+    // Methods
+
+    /**
+     * Gets the documentation.
+     *
+     * @return The ArrayList of documentation
+     */
+    public ArrayList<String> getDocumentation() {
+        // TODO: Method logic
+        return documentation;
+    }
+
+    /**
+     * Sets the documentation.
+     *
+     * @param documentation The ArrayList of documentation to set
+     */
+    public void setDocumentation(ArrayList<String> documentation) {
+        // TODO: Method logic
+        this.documentation = documentation;
+    }
+}
