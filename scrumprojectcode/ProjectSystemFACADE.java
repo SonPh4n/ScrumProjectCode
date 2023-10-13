090f926b442988f807636a22c446d694d5004568
Jane <jedalton@email.sc.edu>
2023-10-13 17:12:40 -0400

    fixed #42

--- scrumprojectcode/ProjectSystemFACADE.java
+++ scrumprojectcode/ProjectSystemFACADE.java
@@ -2,5 +2,53 @@ package scrumprojectcode;
 
 public class ProjectSystemFACADE {
     private User user;
-    private ProjectSystemFACADE facade;
+    private static ProjectSystemFACADE facade;
+
+    public ProjectSystemFACADE(){
+        //TODO
+        user = new User();
+    }
+
+    public ProjectSystemFACADE getInstance(){
+        if(facade == null){
+            facade = new ProjectSystemFACADE();
+        }
+        return facade;
+    }
+
+    public User login(String username, String password){
+        return null;
+    }
+
+    public User register(){
+        return null;
+    }
+
+    public void addProject(Project project){
+        //TODO
+    }
+
+    public void removeProject(Project projet){
+        //TODO
+    }
+
+    public void addColumn(Column column){
+        //TODO
+    }
+
+    public void removeColumn(Column column){
+        //TDO
+    }
+
+    public void addTask(Task task){
+        //TODO
+    }
+
+    public void removeTask(Task task){
+        //TODO
+    }
+
+    public void moveTask(Task task, Column newColumn){
+        //TODO
+    }
 }
