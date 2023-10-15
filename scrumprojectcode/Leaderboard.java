a34d7d2bcd043c960fa325cfb3f8c28f26275aad
AidanH002 <112006206+AidanH002@users.noreply.github.com>
2023-10-15 13:04:06 -0400

    Fixed #27 and #28

--- scrumprojectcode/Leaderboard.java
+++ scrumprojectcode/Leaderboard.java
@@ -2,25 +2,39 @@ package scrumprojectcode;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a leaderboard that displays user scores for a scrum project.
+ */
 public class Leaderboard {
     // Attributes
-    private ArrayList<User> usersLeaderboard;
+    private ArrayList<User> usersLeaderboard;  // Stores the list of users in the leaderboard
 
-    // Constructor
+    /**
+     * Constructor to create a Leaderboard object.
+     *
+     * @param users The list of users to initialize the leaderboard with.
+     */
     public Leaderboard(ArrayList<User> users) {
         // Initialize the leaderboard with the provided users
         this.usersLeaderboard = new ArrayList<>(users);
     }
 
-    // Methods
-
-    // Method to retrieve user scores
+    /**
+     * Method to retrieve and display user scores.
+     *
+     * Implement logic to retrieve user scores and display or process them.
+     * This method should be used to obtain and handle user scores.
+     */
     public void getUserScores() {
         // Implement logic to retrieve user scores
         // This method should display or process user scores
     }
 
-    // Override toString() method
+    /**
+     * Returns a string representation of the Leaderboard object.
+     *
+     * @return A string representing the Leaderboard object.
+     */
     @Override
     public String toString() {
         // Implement logic to convert the leaderboard to a string representation
@@ -29,3 +43,4 @@ public class Leaderboard {
                 '}';
     }
 }
+
