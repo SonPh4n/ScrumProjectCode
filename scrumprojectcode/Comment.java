9bc47b71147907dbb93e742af7d45c42a49ec2a4
AidanH002 <112006206+AidanH002@users.noreply.github.com>
2023-10-15 12:19:29 -0400

    Fixed #21 and #23

--- scrumprojectcode/Comment.java
+++ scrumprojectcode/Comment.java
@@ -3,31 +3,52 @@ package scrumprojectcode;
 import java.util.ArrayList;
 import java.util.UUID;
 
+/**
+ * Represents a comment in a scrum project.
+ */
 public class Comment {
 
     // Attributes
-    private String comment;
-    private User user;
-    private ArrayList<Comment> moreComments;
-    private UUID commentUUID;
+    private String comment;  // Stores the comment text
+    private User user;  // Represents the user associated with the comment
+    private ArrayList<Comment> moreComments;  // Stores Array of comments
+    private UUID commentUUID;  // Provides a unique identifier for the comment
 
-    // Constructors
+    /**
+     * Constructor to create a Comment object.
+     *
+     * @param comment The comment text.
+     * @param user    The user associated with the comment.
+     */
     public Comment(String comment, User user) {
         // Constructor logic
+        this.comment = comment;
+        this.user = user;
+        this.commentUUID = generateUUID();  // Generate a unique UUID for the comment
+        this.moreComments = new ArrayList<>();  // Initialize the list for additional comments
     }
 
-    // Private method to generate UUID
+    /**
+     * Generates a unique UUID for the comment.
+     *
+     * @return The generated UUID.
+     */
     private UUID generateUUID() {
-        // UUID generation logic
+        // UUID generation logic (to be implemented)
+        // This method should return a unique UUID for the comment
         return null;  // Replace with actual logic
     }
 
-    // Override toString method
+    /**
+     * Returns a string representation of the Comment object.
+     *
+     * @return A string representing the Comment object.
+     */
     @Override
     public String toString() {
-        // toString method logic
+        // toString method logic (to be implemented)
+        // This method should return a meaningful string representation of the Comment object
         return "";  // Replace with actual logic
     }
-
-    // Other methods (if needed)
 }
+
