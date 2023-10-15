76b7cedc620dfb722858887f1463d7ad5b22e0d7
Son Phan <sonphan@Sons-MacBook-Air-2.local>
2023-10-15 12:41:10 -0400

    fixed #17

---
+++ scrumprojectcode/DataLoader.java
@@ -0,0 +1,33 @@
+package scrumprojectcode;
+
+import java.util.ArrayList;
+
+public class DataLoader {
+    private ArrayList<Task> tasks;
+    private ArrayList<User> users;
+    private ArrayList<Project> projects;
+
+    public DataLoader()
+    {
+        this.tasks = new ArrayList<>();
+        this.users = new ArrayList<>();
+        this.projects = new ArrayList<>();
+    }
+
+    public ArrayList<Task> getTasks()
+    {
+        
+    }
+
+    public ArrayList<Task> getUsers()
+    {
+    
+    }
+
+    public ArrayList<Task> getProjects()
+    {
+
+    }
+
+    
+}
