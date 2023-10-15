01b06bed7101cbf141d7079daec46f4a1e258a6a
AidanH002 <112006206+AidanH002@users.noreply.github.com>
2023-10-15 12:44:19 -0400

    Fixed #24 and #25

--- scrumprojectcode/History.java
+++ scrumprojectcode/History.java
@@ -2,19 +2,35 @@ package scrumprojectcode;
 
 import java.util.Date;
 
+/**
+ * Represents the history of a task in a scrum project.
+ */
 public class History {
-    private User user;
-    private String details;
-    private Date date;
 
+    private User user;      // Represents the user associated with this history entry
+    private String details; // Stores details of the history entry
+    private Date date;      // Represents the date of the history entry
+
+    /**
+     * Constructor to create a History object.
+     *
+     * @param user    The user associated with this history entry.
+     * @param details Details of the history entry.
+     * @param date    The date of the history entry.
+     */
     public History(User user, String details, Date date) {
-        // Constructor logic
+        // Initialize the History object with provided parameters
     }
 
+    /**
+     * Returns a string representation of the History object.
+     *
+     * @return A string representing the History object.
+     */
     @Override
     public String toString() {
-        // toString method logic
+        // Override the toString() method
+        // of the History object
+        return null;  // Replace with actual logic
     }
-
-    // Other methods if needed
 }
