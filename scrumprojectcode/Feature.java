875e407e06b6f048d3ad6901a57aa8ea6f9f08a2
Jane <jedalton@email.sc.edu>
2023-10-15 17:16:43 -0400

    updated method to match UML

---
+++ scrumprojectcode/Feature.java
@@ -0,0 +1,23 @@
+package scrumprojectcode;
+
+import java.util.ArrayList;
+
+public class Feature {
+    private ArrayList<String> subTasks;
+    
+    public Feature(String taskName, ArrayList<String> subTasks){
+        //TODO
+    }
+
+    public void addSubtas(String subtask){
+        //TODO
+    }
+
+    public void removeSubtask(String subtask){
+        //TODO
+    }
+
+    public String toString(){
+        //TODO
+    }
+}
