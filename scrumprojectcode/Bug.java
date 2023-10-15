02820413c27dd3440d9f00fe0970c955850e9410
Jane <jedalton@email.sc.edu>
2023-10-15 13:55:36 -0400

    imported arraylist

--- scrumprojectcode/Bug.java
+++ scrumprojectcode/Bug.java
@@ -1,9 +1,11 @@
 package scrumprojectcode;
 
-public class Bug extends Task{
+import java.util.ArrayList;
+
+public class Bug extends Task {
 
     public User tester;
-    public Priority priority; 
+    public Priority priority;
     public ArrayList<String> reproductionSteps;
 
     public Bug(String taskName, Priority priority, User tester){
