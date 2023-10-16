02ccd11aca71ba1c0108c039c97ac44e771dcda7
Marshall Kuriakose <kuriakose_ma@mailfence.com>
2023-10-15 23:53:51 -0400

    Updated #36

--- scrumprojectcode/Project.java
+++ scrumprojectcode/Project.java
@@ -99,13 +99,11 @@ public class Project {
     /**
      * String method that prints out the contents of the selected column using
      * ArrayList.forEach() method
+     * 
+     * @return String value of the columnName and columnTasks
      */
-<<<<<<< Updated upstream
-    public String displayColumnTasks(Column column) {
-=======
     public String displayColumnTasks(Column column) { // @author jedalto slightly edited this to compile, should work
                                                       // the same
->>>>>>> Stashed changes
         String tasksToString = "";
         for (Task task : column.columnTasks)
             tasksToString = tasksToString + "- " + column.columnTasks.getTaskName() + "\n";
