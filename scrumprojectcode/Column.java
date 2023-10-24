118157ab7dfeaf3b5eabd032a24f1bec01fe3145
Jane <jedalton@email.sc.edu>
2023-10-23 23:08:19 -0400

    implemented changes to Column class

--- scrumprojectcode/Column.java
+++ scrumprojectcode/Column.java
@@ -3,17 +3,23 @@ package scrumprojectcode;
 import java.util.ArrayList;
 import java.util.UUID;
 
-/**
- * Column: Main class code that holds an ArrayList<Task> with tasks for the
- * of the Project class
- * 
- * @author SonPh4n
- * @author kuriakm
-  */
 public class Column {
     public String columnName;
-    public ArrayList<Task> columnTasks;
+    public ArrayList<UUID> columnTasks; // Preemptively changed this to ArrayList<UUID> to make it easier to read tasks
+                                        // from column.json @kuriakm
     private UUID columnUUID;
+    private UUID projectUUID;
+
+    /**
+     * Debugger method, remove in final submission
+     */
+
+    Column(Project project) {
+        this.projectUUID = project.getProjectUUID();
+        this.columnUUID = generateUUID();
+        this.columnName = "Test column name";
+        this.columnTasks = new ArrayList<>();
+    }
 
     /**
      * Constructor method for a new Column() which initializes the Column attributes
@@ -21,25 +27,22 @@ public class Column {
      * @param columnName
      * @param task
      */
-    public Column(String columnName, Task task) {
+    public Column(String columnName, UUID taskUUID) {
         this.createColumn(columnName);
-        this.columnTasks = new ArrayList<Task>();
-        this.addTask(task);
+        this.columnTasks = new ArrayList<UUID>();
+        this.addTask(taskUUID);
         this.columnUUID = generateUUID();
     }
 
-    /**
-     * Constructor method for a Column() loaded from a file and initializes the
-     * column attributes with the file contents
-     * 
-     * @param columnUUID  UUID generated when the column was initially created
-     * @param columnName  String name of the column
-     * @param columnTasks ArrayList<Task> that holds the tasks of the project column
-     */
-    public Column(String columnUUID, String columnName, ArrayList<String> columnTasks) {
-        this.columnUUID = toUUID(columnUUID); //convert String UUID to object UUID
-        this.columnName = columnName;
-        this.columnTasks = UUIDtoTasks(columnTasks); //converts arrayList of String UUIDs to Task objects
+    public Column(UUID projectID, UUID columnID, String columnTitle, ArrayList<UUID> columnTasks) {
+        this.projectUUID = projectID;
+        this.columnUUID = columnID;
+        this.columnName = columnTitle;
+        this.columnTasks = columnTasks;
+    }
+
+    public UUID generateUUID() {
+        return UUID.randomUUID();
     }
 
     /**
@@ -52,48 +55,12 @@ public class Column {
     }
 
     /**
-     * String method that returns the current value of columnName
-     * 
-     * @return String value of the column's name
-     */
-    public String getColumnName() {
-        return this.columnName;
-    }
-
-    /**
-     * UUID method that generates a value for columnUUID
-     * 
-     * @return UUID value that is set as the column's unique identifier
-     */
-    private UUID generateUUID() {
-        return UUID.randomUUID();
-    }
-
-    /**
-     * UUID method that returns the current value of columnUUID
-     *
-     * @return UUID value that acts as the column's unique identifier
-     */
-    public UUID getColumnUUID() {
-        return this.columnUUID;
-    }
-
-    /**
-     * ArrayList<Task> method that return the current value of columnTasks
-     * 
-     * @return ArrayList<Task> with the column's tasks
-     */
-    public ArrayList<Task> getColumnTasks() {
-        return this.columnTasks;
-    }
-
-    /**
      * Void method that adds a new Task to the Column using the ArrayList.add()
      * method
      * 
      * @param task New project Task to be added to columnTasks
      */
-    private void addTask(Task task) {
+    private void addTask(UUID task) {
         this.columnTasks.add(task);
     }
 
@@ -103,33 +70,49 @@ public class Column {
      * 
      * @param task Task to be removed from the column
      */
-    private void removeTask(Task task) {
+    private void removeTask(UUID task) {
         this.columnTasks.remove(task);
     }
 
-    /**
-     * String method that returns the value of columnName with the tasks underneath
-     * 
-     * @return String value of the columnName and columnTasks
-     */
+    public String getColumnName() {
+        return this.columnName;
+    }
+
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+
+    public ArrayList<UUID> getColumnTasks() {
+        return this.columnTasks;
+    }
+
+    public void setColumnTasks(ArrayList<UUID> columnTasks) {
+        this.columnTasks = columnTasks;
+    }
+
+    public UUID getColumnUUID() {
+        return this.columnUUID;
+    }
+
+    public void setColumnUUID(UUID columnUUID) {
+        this.columnUUID = columnUUID;
+    }
+
+    public UUID getProjectUUID() {
+        return this.projectUUID;
+    }
+
+    public void setProjectUUID(UUID projectUUID) {
+        this.projectUUID = projectUUID;
+    }
+
     public String toString() {
         String tasksToString = "";
-        for (Task task : columnTasks)
-            tasksToString = tasksToString + "- " + task.getTaskName() + "\n";
+        /*
+         * for (Task task : columnTasks)
+         * tasksToString = tasksToString + "- " + task.getTaskName() + "\n";
+         */
         return "--- " + this.columnName + " ---\n" + tasksToString;
     }
-
-    private UUID toUUID(String columnUUID){
-        //TODO convert string UUID from dataloader to object UUID
-        return null;
-    }
-
-    private ArrayList<Task> UUIDtoTasks(ArrayList<String> columnTasks){
-        //TODO convert String UUIDs from dataLoader to Task Objects
-        return null;
-    }
-
-    //getters and setters for all attributes
-    
 
 }
