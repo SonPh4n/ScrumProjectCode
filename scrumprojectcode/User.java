920fda522cf7fc4b2bbd65f6c85295871286f0be
Jane <jedalton@email.sc.edu>
2023-10-23 23:05:37 -0400

    implemented changes to User class

--- scrumprojectcode/User.java
+++ scrumprojectcode/User.java
@@ -1,15 +1,11 @@
 package scrumprojectcode;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.UUID;
 
 /**
- * Changes
- * 
  * @author SonPh4n
  * @author kuriakm
- * @author jedalto
  */
 public class User {
 
@@ -23,9 +19,9 @@ public class User {
     private String type;
 
     private UUID userUUID;
-
-    private ArrayList<Task> myTasks;
-    private ArrayList<Project> myProjects;
+    private ArrayList<UUID> myTasks; // Was unable to use Task and Project object when loading from user.json so I
+                                     // changed these to ArrayList<UUID> @kuriakm
+    private ArrayList<UUID> myProjects;
 
     public User(String firstName, String lastName, String username, String password, String email, String phoneNumber,
             String type) {
@@ -41,8 +37,7 @@ public class User {
     }
 
     public User(UUID userID, String firstName, String lastName, String username, String password, String email,
-            String phoneNumber,
-            String type, ArrayList<UUID> tasks, ArrayList<UUID> projects) {
+            String phoneNumber, String type, ArrayList<UUID> tasks, ArrayList<UUID> projects) {
         this.userUUID = userID;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -50,15 +45,14 @@ public class User {
         this.password = password;
         this.email = email;
         this.phoneNumber = phoneNumber;
-        this.myTasks = UUIDtoTasks(tasks); // arraylist of Task objects
-        this.myProjects = UUIDtoProjects(projects); // arraylist of Project objects
+        this.myTasks = tasks;
+        // this.myTasks = new ArrayList<Task>();
+        // myTasks = UUIDtoTasks(tasks); // arraylist of Task objects
+        this.myProjects = projects;
+        // this.myProjects = new ArrayList<Project>();
+        // myProjects = UUIDtoProjects(projects); // arraylist of Project objects
         this.loggedIn = false;
         this.type = type;
-    }
-
-    // generates a new UUID for new Users
-    private UUID generateUUID() {
-        return UUID.randomUUID();
     }
 
     // Setters and Getters
@@ -118,19 +112,19 @@ public class User {
         this.loggedIn = loggedIn;
     }
 
-    public ArrayList<Task> getMyTasks() {
+    public ArrayList<UUID> getMyTasks() {
         return myTasks;
     }
 
-    public void setMyTasks(ArrayList<Task> myTasks) {
+    public void setMyTasks(ArrayList<UUID> myTasks) {
         this.myTasks = myTasks;
     }
 
-    public ArrayList<Project> getMyProjects() {
+    public ArrayList<UUID> getMyProjects() {
         return myProjects;
     }
 
-    public void setMyProjects(ArrayList<Project> myProjects) {
+    public void setMyProjects(ArrayList<UUID> myProjects) {
         this.myProjects = myProjects;
     }
 
@@ -151,16 +145,12 @@ public class User {
     }
     // end of setters and getters
 
-    // prints public User attributes
-    public String toString() {
-        return "[" + this.username + "]:\n"
-                + "Type: " + this.type + "\n"
-                + "Name: " + this.firstName + " " + this.lastName + "\n"
-                + "Logged In: " + this.loggedIn;
+    private UUID generateUUID() {
+        // check if it has an ID, if from dataloader must convert using toUUID
+        return UUID.randomUUID();
     }
 
-    // prints full User attributes
-    private String printFullAttributes() {
+    public String toString() {
         return "[" + this.username + "]:\n"
                 + "Type: " + this.type + "\n"
                 + "Name: " + this.firstName + " " + this.lastName + "\n"
@@ -200,68 +190,97 @@ public class User {
         return false;
     }
 
-    private boolean addProject(String projectName) {
-        if (myProjects == null) {
-            myProjects = new ArrayList<>();
-        }
-        myProjects.add(new Project(projectName));
-        return true;
+    // Temp method to prevent duplicate Users with different UUIDs from being added
+    // to user.json
+    public boolean equals(User aU) {
+        return this.username.equals(aU.getUsername()) &&
+                this.password.equals(aU.getPassword()) &&
+                this.firstName.equals(aU.getFirstName()) &&
+                this.lastName.equals(aU.getLastName()) &&
+                this.email.equals(aU.getEmail()) &&
+                this.phoneNumber.equals(aU.getPhoneNumber()) &&
+                this.type.equals(aU.getUserType());
     }
 
-    private Project findProject(String projectName) {
-        for (Project project : myProjects) {
-            if (project.getProjectName().equals(projectName)) {
-                return project;
-            }
-        }
-        return null;
-    }
+    /*
+     * public ArrayList<Project> UUIDtoProjects(ArrayList<UUID> projectsUUID) {
+     * Project testProject = new Project();
+     * myProjects.add(testProject);
+     * return myProjects;
+     * }
+     * 
+     * public ArrayList<Task> UUIDtoTasks(ArrayList<UUID> tasksUUID) {
+     * Task testTask = new Task();
+     * myTasks.add(testTask);
+     * return myTasks;
+     * }
+     */
 
-    private ArrayList<Task> UUIDtoTasks(ArrayList<UUID> personalTasks) {
-        // TODO populate myTasks with Task objects based on the UUIDs from personalTasks
-        ArrayList<Task> tasks = new ArrayList<>();
-        for (UUID taskUUID : personalTasks) {
-            Task task = findTaskByUUID(taskUUID);
-            if (task != null) {
-                tasks.add(task);
-            }
-        }
-        return tasks;
-    }
+    /**
+     * private boolean addProject(String projectName) {
+     * if (myProjects == null) {
+     * myProjects = new ArrayList<Project>();
+     * }
+     * listOfProjects.add(projectName);
+     * }
+     * 
+     * private Project findProject(String projectName) {
+     * for (Project project : myProjects) {
+     * if (project.getProjectName().equals(projectName)) {
+     * return project;
+     * }
+     * }
+     * return null;
+     * }
+     * 
+     * private ArrayList<Task> UUIDtoTasks(ArrayList<String> personalTasks) {
+     * // TODO populate myTasks with Task objects based on the UUIDs from
+     * personalTasks
+     * ArrayList<Task> tasks = new ArrayList<>();
+     * for (String taskUUID : personalTasks) {
+     * Task task = findTaskByUUID(taskUUID);
+     * if (task != null) {
+     * tasks.add(task);
+     * }
+     * }
+     * return tasks;
+     * }
+     * 
+     * private Task findTaskByUUID(String taskUUID) {
+     * for (Task task : myTasks) {
+     * if (task.getTaskUUID().toString().equals(taskUUID)) {
+     * return task;
+     * }
+     * }
+     * return null;
+     * }
+     * 
+     * private ArrayList<Project> UUIDtoProjects(ArrayList<String> listOfProjects) {
+     * // TODO populate myProjects with Project objects based on the UUIDs from
+     * // listOfProjects
+     * ArrayList<Project> projects = new ArrayList<>();
+     * for (String projectUUID : listOfProjects) {
+     * Project project = findProjectByUUID(projectUUID);
+     * if (project != null) {
+     * projects.add(project);
+     * }
+     * }
+     * return null;
+     * }
+     * 
+     * private Project findProjectByUUID(String projectUUID) {
+     * for (Project project : myProjects) {
+     * if (project.getProjectUUID().toString().equals(projectUUID)) {
+     * return project;
+     * }
+     * }
+     * return null;
+     * }
+     * 
+     * private UUID toUUID(String userID) {
+     * // TODO convert string UUID from dataLoader to object UUID
+     * return UUID.fromString(userID);
+     * }
+     */
 
-    private Task findTaskByUUID(UUID taskUUID) {
-        for (Task task : myTasks) {
-            if (task.getTaskUUID().equals(taskUUID)) {
-                return task;
-            }
-        }
-        return null;
-    }
-
-    private ArrayList<Project> UUIDtoProjects(ArrayList<UUID> listOfProjects) {
-        // TODO populate myProjects with Project objects based on the UUIDs from
-        // listOfProjects
-        ArrayList<Project> projects = new ArrayList<>();
-        for (UUID projectUUID : listOfProjects) {
-            Project project = findProjectByUUID(projectUUID);
-            if (project != null) {
-                projects.add(project);
-            }
-        }
-        return projects;
-    }
-
-    private Project findProjectByUUID(UUID projectUUID) {
-        for (Project project : myProjects) {
-            if (project.getProjectUUID().equals(projectUUID)) {
-                return project;
-            }
-        }
-        return null;
-    }
-
-    // converts string UUID from dataLoader to object UUID
-    private UUID toUUID(String userID) {
-        return UUID.fromString(userID);
-    }
 }
