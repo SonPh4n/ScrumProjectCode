9cb8bc3a12ae55067830cb8f1753aa1ed5381e30
Son Phan <sonphan@Sons-MacBook-Air-2.local>
2023-10-25 17:03:49 -0400

    fixed #74

--- scrumprojectcode/Leaderboard.java
+++ scrumprojectcode/Leaderboard.java
@@ -1,6 +1,8 @@
 package scrumprojectcode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Represents a leaderboard that displays user scores for a scrum project.
@@ -8,6 +10,7 @@ import java.util.ArrayList;
 public class Leaderboard {
     // Attributes
     private ArrayList<User> usersLeaderboard;  // Stores the list of users in the leaderboard
+    private Map<User, Integer> userScores;     // Stores user scores
 
     /**
      * Constructor to create a Leaderboard object.
@@ -15,8 +18,13 @@ public class Leaderboard {
      * @param users The list of users to initialize the leaderboard with.
      */
     public Leaderboard(ArrayList<User> users) {
-        // Initialize the leaderboard with the provided users
         this.usersLeaderboard = new ArrayList<>(users);
+        this.userScores = new HashMap<>();
+        
+        // Initialize user scores to zero for each user
+        for (User user : usersLeaderboard) {
+            userScores.put(user, 0);
+        }
     }
 
     /**
@@ -25,9 +33,29 @@ public class Leaderboard {
      * Implement logic to retrieve user scores and display or process them.
      * This method should be used to obtain and handle user scores.
      */
+    public void setUserScore(User user, int score) {
+        if (userScores.containsKey(user)) {
+            userScores.put(user, score);
+        } else {
+            System.out.println("User not found in the leaderboard.");
+        }
+    }
+
     public void getUserScores() {
-        // Implement logic to retrieve user scores
-        // This method should display or process user scores
+        for (User user : usersLeaderboard) {
+            int score = userScores.get(user);
+            System.out.println("User: " + user.getFirstName() + " Score: " + score);
+        }
+    }
+
+    public void completeTask(User user, int taskPoints) {
+        if (userScores.containsKey(user)) {
+            int currentScore = userScores.get(user);
+            int newScore = currentScore + taskPoints;
+            userScores.put(user, newScore);
+        } else {
+            System.out.println("User not found in the leaderboard.");
+        }
     }
 
     /**
@@ -37,10 +65,12 @@ public class Leaderboard {
      */
     @Override
     public String toString() {
-        // Implement logic to convert the leaderboard to a string representation
-        return "Leaderboard{" +
-                "usersLeaderboard=" + usersLeaderboard +
-                '}';
+        String leaderboardString = "Leaderboard:\n";
+        for (User user : usersLeaderboard) {
+            int score = userScores.get(user);
+            leaderboardString += "User: " + user.getFirstName() + " Score: " + score + "\n";
+        }
+    return leaderboardString;
     }
 }
 
