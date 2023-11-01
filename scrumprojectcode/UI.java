da9cbc4615ab01146e60147ec255ab37fd1a473b
Jane <jedalton@email.sc.edu>
2023-11-01 18:51:37 -0400

    working on #80

--- scrumprojectcode/UI.java
+++ scrumprojectcode/UI.java
@@ -14,104 +14,74 @@ public class UI {
 
     public void run() {
         scenario1();
-        scenario2();
     }
 
     public void scenario1() {
         System.out.println();
 
-        // User logs in
-        /*
-         * if (!facade.login("johnny", "8xw0CUeKk")) {
-         * System.out.println("Sorry we couldn't login.");
-         * return;
-         * }
-         */
         if (!facade.login("a_madden", "O4r40Rs")) {
             System.out.println("Sorry we couldn't log you in.");
         } else {
             System.out.println("Atticus Madden is now logged in");
         }
-        // System.out.println("Johnny Applesause is now logged in.");
 
-        // make a project
-        if (!facade.addProject("Electric Missile")) {
-            System.out.println("Sorry we couldn't create your project.");
-        } else {
-            System.out.println("'Electric Missile' has been successfully created.");
-        }
-        // make a project
-        if (!facade.addProject("Soap Free Washers")) {
-            System.out.println("Sorry we couldn't create your project.");
-        } else {
-            System.out.println("'Soap Free Washers' has been successfully created.");
-        }
-        // make a project
-        if (!facade.addProject("Air Computers")) {
-            System.out.println("Sorry we couldn't create your project.");
-        } else {
-            System.out.println("'Air Computers' has been successfully created.");
-        }
-
-        // create column
-        if (!facade.addColumn("Electric Missile", "Back Log")) {
-            System.out.println("Sorry we couln't create your column.");
-        } else {
-            System.out.println("Back Log has been added to Electric Missile");
-        }
-        // create column
-        if (!facade.addColumn("Electric Missile", "To Do")) {
-            System.out.println("Sorry we couln't create your column.");
-        } else {
-            System.out.println("To Do has been added to Electric Missile");
-        }
+        System.out.println("You have opened the project 'Electric Missile'");
 
         // create task
         if (!facade.addTask("Electric Missile", "Back Log", "task2",
                 "Initialize super algorithm to detonate at warp speed", "task", "11/\\01/\\2023")) {
             System.out.println("Sorry, we could not create your task.");
         } else {
-            System.out.println("A task has been added to Back Log in Electric Missile.");
-        }
-        // create task
-        if (!facade.addTask("Electric Missile", "Back Log", "task1", "Curve the metal to make a cylindrical shape",
-                "task", "11/\\01/\\2023")) {
-            System.out.println("Sorry, we could not create your task.");
-        } else {
-            System.out.println("A task has been added to Back Log in Electric Missile.");
+            System.out.println("Task: 'Initialize super algorithm to detonate at warp speed' has been added to the project.");
         }
 
+        //assign jeff to task
         if (!facade.removeUserFromTask("a_madden", "task2") && !facade.addUserToTask("gold_j", "task2")) {
             System.out.println("Sorry, we could not add an assigned user");
         } else {
-            System.out.println("Assigned task to Jeff Goldblum");
+            System.out.println("Jeff Goldblum has been assigned to 'Initialize super algorition to detonate warp speed'.");
         }
 
-        if (!facade.addComment("gold_j", "task1", "Not cylindrical enough")) {
+        //user comments "avoid civilians jeff" to warp task
+        
+        if(!facade.addComment("a_madden", "task2", "Avoid civilians Jeff!")) {
             System.out.println("Sorry, we could not add your comment to the task");
         } else {
-            System.out.println("new comment has been added to the task");
+            System.out.println("'Avoid civilians Jeff!' comment has been added to task");
         }
 
-        if (!facade.addComment("a_madden", "task1", "What's a cylinder")) {
-            System.out.println("Sorry, we could not add your comment to the task");
-        } else {
-            System.out.println("new comment has been added to the task");
-        }
-
-        if (!facade.addReplyComment("a_madden", "task1", "Avoid civilians Jeff!", "gold_j", "Not cylindrical enough")) {
-            System.out.println("Sorry, we could not add your comment to the task");
-        } else {
-            System.out.println("new comment has been added to the task");
-        }
-
-        if (!facade.moveTask("Electric Missile", "Back Log", "To Do", "task1")) {
+        //cylindrical task moved to Doing
+        if (!facade.moveTask("Electric Missile", "Back Log", "Doing", "task1")) {
             System.out.println("Sorry, we could not move your task to the column");
         } else {
-            System.out.println("'task has been moved to 'To Do' column");
+            System.out.println("Task: 'Curve the metal to make a cylindrical shape' has been moved to 'Doing'");
+        }
+
+        /////add comment to existing task
+        if(!facade.addReplyComment("a_madden", "task1", "How about you do it Jeff?", "gold_j", "Not cylindrical enough")){
+            System.out.println("Sorry, we could not add your comment to the task");
+        } else {
+            System.out.println("'How about you do it Jeff?' has been added as a reply to gold_j");
+        }
+        
+        //create abandoned column
+        if (!facade.addColumn("Electric Missile", "Abandoned")) {
+            System.out.println("Sorry we couln't create your column.");
+        } else {
+            System.out.println("'Abandoned' has been added to Electric Missile");
+        } 
+
+        //move impossible burger task to abandoned
+        if (!facade.moveTask("Electric Missile", "Back Log", "Abandoned", "task3")) {
+            System.out.println("Sorry, we could not move your task to the column");
+        } else {
+            System.out.println("Task: 'Make impossible burger possible' has been moved to 'Abandoned'");
         }
 
         facade.logout();
+        if(facade.getCurrentUser() == null){
+            System.out.println("Atticus Madden is now logged out.");
+        }
 
     }
 
