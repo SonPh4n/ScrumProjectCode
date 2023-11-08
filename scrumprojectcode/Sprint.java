ce87981760aa030107f13cbfab302527ab3387b1
kuriakm <kuriakose_ma@mailfence.com>
2023-11-08 16:40:21 -0500

    Working on #94

--- scrumprojectcode/Sprint.java
+++ scrumprojectcode/Sprint.java
@@ -1,8 +1,10 @@
 package scrumprojectcode;
+
 import java.util.ArrayList;
 
 /**
- * The Sprint class represents a sprint in a Scrum project. A sprint has a start date, an end date, and a list of tasks to complete.
+ * The Sprint class represents a sprint in a Scrum project. A sprint has a start
+ * date, an end date, and a list of tasks to complete.
  */
 public class Sprint {
     private String startDate;
@@ -10,16 +12,17 @@ public class Sprint {
     private ArrayList<Task> tasksToComplete;
 
     /**
-     * Constructs a new Sprint object with the given start date, end date, and list of tasks to complete.
+     * Constructs a new Sprint object with the given start date, end date, and list
+     * of tasks to complete.
      *
-     * @param startDate the start date of the sprint
-     * @param endDate the end date of the sprint
+     * @param startDate       the start date of the sprint
+     * @param endDate         the end date of the sprint
      * @param tasksToComplete the list of tasks to complete in the sprint
      */
     public Sprint(String startDate, String endDate, ArrayList<Task> tasksToComplete) {
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.tasksToComplete = tasksToComplete;
+        setStartDate(startDate);
+        setEndDate(endDate);
+        setTasksToComplete(tasksToComplete);
     }
 
     /**
@@ -40,8 +43,75 @@ public class Sprint {
         tasksToComplete.remove(task);
     }
 
+    // Added setters and getters for attributes to make testing easier @kuriakm
+
     /**
-     * Returns a string representation of the sprint, including its start date, end date, and list of tasks to complete.
+     * String method that returns the current value of startDate
+     * 
+     * @return String representation of the sprint's start date
+     */
+    public String getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * Void method that initializes this.startDate with the param startDate
+     * 
+     * @param startDate String that is set to this.startDate
+     */
+    public void setStartDate(String startDate) {
+        if (startDate != null)
+            this.startDate = startDate;
+        else
+            this.startDate = "N/A";
+    }
+
+    /**
+     * String method that returns the current value of endDate
+     * 
+     * @return String representation of the sprint's end date
+     */
+    public String getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * Void method that initializes this.endDate with the param endDate
+     * 
+     * @param endDate String that is set to this.endDate
+     */
+    public void setEndDate(String endDate) {
+        if (endDate != null)
+            this.endDate = endDate;
+        else
+            this.endDate = "N/A";
+    }
+
+    /**
+     * ArrayList<Task> method that returns the current value of tasksToComplete
+     * 
+     * @return ArrayList<Task> of current tasks in the sprint
+     */
+    public ArrayList<Task> getTasksToComplete() {
+        return tasksToComplete;
+    }
+
+    /**
+     * Void method that initializes this.tasksToComplete with the param
+     * tasksToComplete
+     * 
+     * @param tasksToComplete ArrayList<Task> that is set to this.tasksToComplete
+     */
+    public void setTasksToComplete(ArrayList<Task> tasksToComplete) {
+        if (tasksToComplete != null)
+            this.tasksToComplete = tasksToComplete;
+        else
+            this.tasksToComplete = new ArrayList<>();
+    }
+
+    /**
+     * Returns a string representation of the sprint, including its start date, end
+     * date, and list of tasks to complete.
      *
      * @return a string representation of the sprint
      */
--- scrumprojectcode/Tests/SprintTest.java
+++ scrumprojectcode/Tests/SprintTest.java
@@ -2,9 +2,12 @@ package scrumprojectcode.Tests;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import scrumprojectcode.DataLoader;
 import scrumprojectcode.Sprint;
 import scrumprojectcode.Task;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.UUID;
@@ -28,15 +31,100 @@ import static org.junit.jupiter.api.Assertions.*;
  * 
  * @author kuriakm
  */
+
 public class SprintTest {
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/YYYY");
+    private Sprint sprint;
+    private String start;
+    private String end;
+    private ArrayList<Task> taskList;
+    private Task[] taskListToArray;
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/YYYY");
 
-    // Test Sprint(String startDate, String endDate, ArrayList<Task>
-    // tasksToComplete)
+    /*
+     * public static void main(String[] args) {
+     * SprintTest st = new SprintTest();
+     * st.testConstructorWithNullStartDate();
+     * }
+     */
 
-    Date startDate = 
+    @BeforeEach
+    void setUp() {
+        LocalDate startDate = LocalDate.now();
+        start = formatter.format(startDate);
+        LocalDate endDate = startDate.plusDays(12);
+        end = formatter.format(endDate);
 
-    // Test addNewTask(Task task): void
+        taskList = DataLoader.loadTasks();
+        taskListToArray = taskList.toArray(new Task[taskList.size()]);
+
+        sprint = new Sprint(start, end, taskList);
+    }
+
+    /**
+     * Test Sprint(String startDate, String endDate, ArrayList<Task>
+     * tasksToComplete) with normal values
+     * 
+     * Everything should return true if the class methods are working
+     * properly @kuriakm
+     */
+    @Test
+    void testConstructor() {
+        assertNotNull(sprint);
+        assertEquals(start, sprint.getStartDate());
+        assertEquals(end, sprint.getEndDate());
+        assertArrayEquals(taskListToArray, sprint.getTasksToComplete().toArray(new Task[taskList.size()]));
+    }
+
+    /*
+     * Test Sprint(String startDate, String endDate, ArrayList<Task>
+     * tasksToComplete) with null date values
+     * 
+     * Should return true as the Sprint should replace these null values with
+     * "N/A" @kuriakm
+     */
+    @Test
+    void testConstructorWithNullDates() {
+        Sprint nullStartDate = new Sprint(null, end, taskList);
+        assertNotNull(nullStartDate.getStartDate());
+
+        Sprint nullEndDate = new Sprint(start, null, taskList);
+        assertNotNull(nullEndDate.getEndDate());
+    }
+
+    /*
+     * Test Sprint(String startDate, String endDate, ArrayList<Task>
+     * tasksToComplete) with null ArrayList<Task>
+     * 
+     * Should return true as the Sprint should replace this null object with an
+     * empty ArrayList @kuriakm
+     */
+    @Test
+    void testConstructorWithNullTasksToComplete() {
+        Sprint nullTasksToComplete = new Sprint(start, end, null);
+        assertNotNull(nullTasksToComplete.getTasksToComplete());
+    }
+
+    /*
+     * Test Sprint(String startDate, String endDate, ArrayList<Task>
+     * tasksToComplete) with null Tasks in tasksToComplete
+     * 
+     * Should return true as the Sprint should remove the null objects and keep the
+     * rest @kuriakm
+     */
+    @Test
+    void testConstructorWithNullTasks() {
+        Task nullTask = null;
+        taskList.add(nullTask);
+        taskList.add(nullTask);
+        Sprint nullTasks = new Sprint(start, end, taskList);
+
+        for (Task task : nullTasks.getTasksToComplete())
+            assertNotEquals(nullTask, task);
+    }
+
+    /**
+     * Test addNewTask(Task task): void
+     */
     // Test removeTask(Task task): void
     // Test toString(): String
 }
